4f4c08bc98488ea92d6f3d82e07463edbc6be03e
JRSanchez <jose.sanchez@dollarcity.com>
2024-05-06 14:58:35 -0500

    upload category data file

--- srv/service.js
+++ srv/service.js
@@ -22,18 +22,47 @@
 
 
 const cds = require('@sap/cds')
-module.exports = function (){
+
+module.exports = (srv) => {
+// module.exports = function  Category() {
+
 //   this.on ('submitOrder', (req)=>{...}) //> custom actions
 //   this.on ('CREATE',`Books`, (req)=>{...})
 //   this.before ('UPDATE',`*`, (req)=>{...})
-
 //   this.after ('READ','CatalogService', (Category)=>{
-//     const newCategories = [];
-//     Category.array.array.forEach(element => {
-//         console.log('Service name')
-//         newCategories.push(element)
-//     });
 
-//     return newCategories;
+srv.on('READ', 'GetCatalog', (req) => {
+// let categ = req.data
+const newCategories = [];
+console.log('Service name')
+
+    // req.data.array.forEach(element => {
+    //     newCategories.push(element)
+    // });
+
+})
+//   this.after ('READ','CatalogService', (Category)=>{
+    // const newCategories = [];
+    // console.log('Service name')
+    // Category.array.array.forEach(element => {
+        
+    //     newCategories.push(element)
+    // });
+
+    // return newCategories;
+    // return Category
 //   })
 }
+
+
+// cds.serve('inventory-service') .with (function(){
+//     this.after('READ', '*', (devices)=>{
+//       for (let each of devices) {
+//         var deviceAge = calculateDeviceAgeYears(each)
+//         if (deviceAge >= 4) {
+//           each.eligible_for_replacement = true
+//         } else {
+//           each.eligible_for_replacement = false
+//         }
+//    }  })
+//   })
